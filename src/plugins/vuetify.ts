e8cf796448bd81d40eb675f01fc07d602b3b4b3c
randomunrandom <danilunrandom@gmail.com>
2019-11-26 23:11:08 +0300

    added i18n support for vuetify

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,26 +1,33 @@
-import Vue from "vue"
-import Vuetify from "vuetify/lib"
-import LRU from "lru-cache"
+import Vue from "vue";
+import Vuetify from "vuetify/lib";
+import LRU from "lru-cache";
+
+import i18n from "./i18n";
 
 Vue.use(Vuetify);
 
 const themeCache = new LRU({
   max: 10,
-  maxAge: 1000 * 60 * 60 * 6,
-})
+  maxAge: 1000 * 60 * 60 * 6
+});
 
-export default new Vuetify({
+let config: any = {
   icons: {
     iconfont: "mdiSvg"
   },
   theme: {
+    // dark: true,
     options: {
       themeCache,
-      minifyTheme: function (css) {
-        return process.env.NODE_ENV === 'production'
-          ? css.replace(/[\r\n|\r|\n]/g, '')
-          : css
-      },
+      minifyTheme: function(css: any) {
+        return process.env.NODE_ENV === "production"
+          ? css.replace(/[\r\n|\r|\n]/g, "")
+          : css;
+      }
     }
+  },
+  lang: {
+    t: (key: string, ...params: any) => i18n.t(key, params)
   }
-});
+};
+export default new Vuetify(config);
