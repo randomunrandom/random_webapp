b27d25fc2ed4d502061a48ea6e5d1e69e54cfd81
randomunrandom <danilunrandom@gmail.com>
2019-11-26 23:15:52 +0300

    minor improvments and code cleanup

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template lang="pug">
   v-app
     v-system-bar(app)
+
     v-navigation-drawer(app v-model="drawer")
       v-row(justify="space-around")
         v-avatar.pt-5.pb-2(size=128)
           v-img(src="https://randomuser.me/api/portraits/men/10.jpg")
-      v-list-item.text-center
+      v-list-item
         v-list-item-content
-          v-list-item-title {{}}
-          v-list-item-subtitle {{}}
-      v-list(nav)
+          v-list-item-title {{ $t('name') }}
+          v-list-item-subtitle {{ $t('title') }}
+      v-list
         v-list-item(
           v-for="(item, i) in navItems"
           :key="i"
@@ -20,6 +21,11 @@
             v-icon {{ item.icon }}
           v-list-item-content
             v-list-item-title {{ item.name }}
+        v-list-item
+          v-switch(
+            v-model="$vuetify.theme.dark"
+            :label="$t('change_theme')"
+          )
 
     v-app-bar(app dense flat)
       v-app-bar-nav-icon(@click.stop="drawer = !drawer")
@@ -27,14 +33,14 @@
       v-toolbar-title {{}}
 
     v-content
-      v-container(fluid)
+      v-container(fluid transition="slide-x-transition")
         router-view
 
     v-footer(app padless)
       v-card.red.lighten-3.text-center(flat tile width='100%')
-        v-card-text.white--text
+        v-card-text
           | &copy; 2019 &mdash;&nbsp;
-          strong Random&nbsp;
+          strong {{ $t('all.name') }}&nbsp;
           | All Rights Reserved
 
     //- div
@@ -49,23 +55,18 @@
           :value="lang['locale']"
           @click="change_lang(lang['locale'])"
           ) {{ lang["name"] }}
-      p
-        router-link(to="home") Go to Home
-      p
-        router-link(to="about") Go to About
-      p {{$t('hello')}}
 
 </template>
 
 <script lang="ts">
   import Vue from "vue";
   import Component from "vue-class-component";
-  import { mdiHome, mdiAccountSupervisorCircle, mdiClockStart } from "@mdi/js";
+  import { mdiHome, mdiInformation, mdiClockStart } from "@mdi/js";
 
-  interface LocaleSetting {
-    locale: string;
-    name: string;
-  }
+  // interface LocaleSetting {
+  // locale: string;
+  // name: string;
+  // }
   interface NavigationItem {
     icon: any;
     name: string;
@@ -75,43 +76,43 @@
   @Component({})
   export default class App extends Vue {
     drawer: Boolean = true;
-    // drawer_colapsed: Boolean = false
+
     navItems: NavigationItem[] = [
       { icon: mdiHome, name: "Home", link: "home" },
-      { icon: mdiAccountSupervisorCircle, name: "About", link: "about" }
+      { icon: mdiInformation, name: "About", link: "about" }
       // {icon: mdiClockStart, name: 'Clock-in', link: ''},
     ];
 
-    locales: LocaleSetting[] = [
-      {
-        locale: "ru",
-        name: "Русский"
-      },
-      {
-        locale: "en",
-        name: "English"
-      }
-    ];
+    // locales: LocaleSetting[] = [
+    // {
+    // locale: "ru",
+    // name: "Русский"
+    // },
+    // {
+    // locale: "en",
+    // name: "English"
+    // }
+    // ];
 
-    available_locales: string[] = this.locales.map((el) => {
-      return el["locale"];
-    });
+    // available_locales: string[] = this.locales.map((el) => {
+    // return el["locale"];
+    // });
 
-    get diff_locales(): LocaleSetting[] {
-      return this.locales.filter((el: LocaleSetting): boolean => {
-        return el.locale !== this.$root.$i18n.locale;
-      });
-    }
+    // get diff_locales(): LocaleSetting[] {
+    // return this.locales.filter((el: LocaleSetting): boolean => {
+    // return el.locale !== this.$root.$i18n.locale;
+    // });
+    // }
 
-    change_lang(locale: string): void {
-      if (this.available_locales.includes(locale)) {
-        let path: string = window.location.pathname;
-        let res: string = path.replace(/(\/)(?:\w+)(\/\S*)/, `$1${locale}$2`);
-        this.$router.push(res);
-      } else {
-        console.log("locale not acceptable");
-      }
-    }
+    // change_lang(locale: string): void {
+    // if (this.available_locales.includes(locale)) {
+    // let path: string = window.location.pathname;
+    // let res: string = path.replace(/(\/)(?:\w+)(\/\S*)/, `$1${locale}$2`);
+    // this.$router.push(res);
+    // } else {
+    // console.log("locale not acceptable");
+    // }
+    // }
   }
 </script>
 
