f6911a94fbb9f06224a2f41bf6873f5197a7a9f5
randomunrandom <danilunrandom@gmail.com>
2019-11-26 23:09:38 +0300

    added lru cache for vuetify theme caching

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,10 +1,26 @@
-import Vue from "vue";
-import Vuetify from "vuetify/lib";
+import Vue from "vue"
+import Vuetify from "vuetify/lib"
+import LRU from "lru-cache"
 
 Vue.use(Vuetify);
+
+const themeCache = new LRU({
+  max: 10,
+  maxAge: 1000 * 60 * 60 * 6,
+})
 
 export default new Vuetify({
   icons: {
     iconfont: "mdiSvg"
+  },
+  theme: {
+    options: {
+      themeCache,
+      minifyTheme: function (css) {
+        return process.env.NODE_ENV === 'production'
+          ? css.replace(/[\r\n|\r|\n]/g, '')
+          : css
+      },
+    }
   }
 });
